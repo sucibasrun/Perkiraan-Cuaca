122619ef860e08bf1fbc1e4e700330bdc3c09efd
Wulan Suci Basrun <wulan.suci7498@sma.belajar.id>
2025-04-28 18:13:57 +0700

    menambahkan beberapa fitur script.js

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const timeE1 = document.getElementById('time');
 const dateE1 = document.getElementById('date');
 const currentweatherItemsE1 = document.getElementById('current-weather-items');
 const timezone = document.getElementById('time-zone');
-const countryE1 = getElementById('country');
+const countryE1 = document.getElementById('country');
 const weatherForecastE1 = document.getElementById('weather-forecast');
 const current = document.getElementById('current-temp');
+
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 setInterval(() => {
     const time = new Date();
@@ -16,6 +20,7 @@ setInterval(() => {
     const minutes = time.getMinutes();
     const ampm = hour >=12 ? 'PM' : 'AM'
 
-    timeE1.innerHTML = hoursIn12Hrsormat + ':' + minutes + ' ' + `<span id="am-pm">${ampm}</span>`
+    timeE1.innerHTML = `${hoursIn12Hrsormat}:${minutes}<span id="am-pm">${ampm}</span>`;
+    dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month];
 
 }, 1000);
